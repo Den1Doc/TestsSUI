c01270ae40e6608309db938c5de83c3aed80577e
Денис Мишин <akkgame1500@mail.ru>
2023-03-31 17:08:19 +0300

    j

---
+++ TextSUI/View/EscapingBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  EscapingBootcamp.swift
+//  TextSUI
+//
+//  Created by Денис Мишин on 31.03.2023.
+//
+
+import SwiftUI
+
+struct EscapingBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EscapingBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        EscapingBootcamp()
+    }
+}
