eb155eded2997fb14e7d728295e0ea93743a18c5
Денис Мишин <akkgame1500@mail.ru>
2023-04-18 11:25:33 +0300

    новое

---
+++ TestsSUI/View/Bootcamp/NSCacheBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  NSCacheBootcamp.swift
+//  TestsSUI
+//
+//  Created by Денис Мишин on 18.04.2023.
+//
+
+import SwiftUI
+
+struct NSCacheBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NSCacheBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        NSCacheBootcamp()
+    }
+}
